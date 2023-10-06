a5140590b30dc6883e7fcf7ad0792ecedcf02555
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-10-06 18:17:05 -0300

    carrosel and login button

--- src/index.js
+++ src/index.js
@@ -32,3 +32,41 @@ const mobileNavbar = new MobileNavbar(
 );
 
 mobileNavbar.init();
+
+
+/*carrosel-por que usar o men plus*/
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
+
+
