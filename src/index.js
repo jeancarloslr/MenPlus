aba91bd632eda00dd37aa411d4b0089e6f218efe
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-10-07 16:00:16 -0300

    carousel; ajuste de tamanho de tela: section e header; efeito hover no botão de login.

--- src/index.js
+++ src/index.js
@@ -35,38 +35,65 @@ mobileNavbar.init();
 
 
 /*carrosel-por que usar o men plus*/
-const controls = document.querySelectorAll(".control");
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
+const carouselContainer = document.querySelector('.carousel-container');
+const carousel = document.querySelector('.carousel');
+const carouselItems = document.querySelectorAll('.carousel-item');
 
-controls.forEach((control) => {
-  control.addEventListener("click", (e) => {
-    isLeft = e.target.classList.contains("arrow-left");
+let currentIndex = 0;
+let startX = 0;
+let isDragging = false;
 
-    if (isLeft) {
-      currentItem -= 1;
-    } else {
-      currentItem += 1;
-    }
-
-    if (currentItem >= maxItems) {
-      currentItem = 0;
-    }
-
-    if (currentItem < 0) {
-      currentItem = maxItems - 1;
-    }
-
-    items.forEach((item) => item.classList.remove("current-item"));
-
-    items[currentItem].scrollIntoView({
-      behavior: "smooth",
-      inline: "center"
-    });
-
-    items[currentItem].classList.add("current-item");
-  });
+carousel.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startX = e.clientX;
+  e.preventDefault(); // Evita a seleção de texto durante o arrasto
 });
 
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  const currentX = e.clientX;
+  const deltaX = startX - currentX;
+  
+  // Ajuste o valor 10 de acordo com a sensibilidade do arrasto desejada
+  if (deltaX > 10 && currentIndex < carouselItems.length - 1) {
+    currentIndex++;
+    isDragging = false;
+  } else if (deltaX < -10 && currentIndex > 0) {
+    currentIndex--;
+    isDragging = false;
+  }
+  
+  const itemWidth = carouselItems[0].offsetWidth;
+  carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+});
 
+carousel.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDragging = false;
+});
+
+// Para suporte a dispositivos móveis (toque/swipe)
+carousel.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchmove', (e) => {
+  const currentX = e.touches[0].clientX;
+  const deltaX = startX - currentX;
+  
+  if (deltaX > 50 && currentIndex < carouselItems.length - 1) {
+    currentIndex++;
+    startX = currentX;
+  } else if (deltaX < -50 && currentIndex > 0) {
+    currentIndex--;
+    startX = currentX;
+  }
+  
+  const itemWidth = carouselItems[0].offsetWidth;
+  carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+});
+
+// Função para atualizar a largura do item com base na largura da tela
